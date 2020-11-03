2470ab069862cf69b29740a4f3147e0768758717
PritikaRamu <60213317+PritikaRamu@users.noreply.github.com>
2020-11-03 13:22:47 +0530

    Added lesson 3

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,19 @@ Rails.application.routes.draw do
    collection do
      get :lesson1
      get :lesson2
+     get :lesson3
+     get :lesson4
+     get :lesson5
+     get :lesson6
+     get :lesson7
+     get :lesson8
+     get :lesson9
+     get :lesson10
+     get :lesson11
+     get :lesson12
+     get :lesson13
+     get :lesson14
+     get :lesson15
    end
  end
  resources :users, except: [:new]
